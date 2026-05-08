2e95cc37599b3ed626f6eba52dc68c5404ef0b81
me <heiler.bemerguy@gmail.com>
2026-05-08 16:19:41 -0300

    Simplify governor redirection to focus only on PID 1 (init)

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -97,10 +97,6 @@ static BLOCKING_NOTIFIER_HEAD(cpufreq_policy_notifier_list);
 SRCU_NOTIFIER_HEAD_STATIC(cpufreq_transition_notifier_list);
 
 static int off __read_mostly;
-static bool bandido_lock = true;
-module_param(bandido_lock, bool, 0644);
-MODULE_PARM_DESC(bandido_lock, "Redirect 'schedutil' to 'Bandido' governor (default: true)");
-
 static int cpufreq_disabled(void)
 {
 	return off;
@@ -594,16 +590,13 @@ static struct cpufreq_governor *find_governor(const char *str_governor)
 
 	if (!strncasecmp(str_governor, "schedutil", CPUFREQ_NAME_LEN)) {
 		/* 
-		 * Redirect to Bandido if:
-		 * 1. bandido_lock is manually enabled (default)
-		 * OR
-		 * 2. We are in the first 5 minutes of boot (init script window)
+		 * Ignore requests from init (PID 1) to switch to schedutil,
+		 * redirecting them to Bandido instead.
 		 */
-		if (bandido_lock || ktime_get_boot_ns() < 300ULL * NSEC_PER_SEC) {
+		if (task_tgid_vnr(current) == 1) {
 			for_each_governor(t) {
 				if (!strncasecmp("Bandido", t->name, CPUFREQ_NAME_LEN)) {
-					pr_info("cpufreq: redirecting 'schedutil' request from %s [%d] to 'Bandido'\n",
-						current->comm, task_tgid_vnr(current));
+					pr_info("cpufreq: redirecting 'schedutil' request from init [1] to 'Bandido'\n");
 					return t;
 				}
 			}
