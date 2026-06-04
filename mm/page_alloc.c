17438388a44aca61b20c3666db84f38056298b4d
me <heiler.bemerguy@gmail.com>
2026-06-04 14:08:01 -0300

    zram, mm: switch default compressor to zstd and raise min_free_kbytes floor to 64MB

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -8026,10 +8026,10 @@ int __meminit init_per_zone_wmark_min(void)
 
 	if (new_min_free_kbytes > user_min_free_kbytes) {
 		min_free_kbytes = new_min_free_kbytes;
-		if (min_free_kbytes < 128)
-			min_free_kbytes = 128;
-		if (min_free_kbytes > 65536)
+		if (min_free_kbytes < 65536)
 			min_free_kbytes = 65536;
+		if (min_free_kbytes > 131072)
+			min_free_kbytes = 131072;
 	} else {
 		pr_warn("min_free_kbytes is not updated to %d because user defined value %d is preferred\n",
 				new_min_free_kbytes, user_min_free_kbytes);
