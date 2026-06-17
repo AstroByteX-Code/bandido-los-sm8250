86663b2a6817171a01c39f361750aea71151d73f
me <heiler.bemerguy@gmail.com>
2026-06-17 12:23:27 -0300

    sched/fair: scale SchedTune margin proportionally to CPU original capacity
    - Pass the target CPU original capacity to schedtune_margin() in schedtune_cpu_margin_with().
    - Scale SchedTune boost margin proportionally to capacity_orig_of(cpu) instead of hardcoded SCHED_CAPACITY_SCALE (1024).
    - Prevent over-boosting and task migration from LITTLE cores.
    - Cast signal to long inside schedtune_margin() to avoid signedness promotion bug when multiplying by signed boost.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6179,7 +6179,7 @@ static int wake_affine(struct sched_domain *sd, struct task_struct *p,
 struct reciprocal_value schedtune_spc_rdiv;
 
 static long
-schedtune_margin(unsigned long signal, long boost)
+schedtune_margin(unsigned long signal, long boost, long capacity)
 {
 	long long margin = 0;
 
@@ -6188,14 +6188,16 @@ schedtune_margin(unsigned long signal, long boost)
 	 *
 	 * The Boost (B) value is used to compute a Margin (M) which is
 	 * proportional to the complement of the original Signal (S):
-	 *   M = B * (SCHED_CAPACITY_SCALE - S)
+	 *   M = B * (capacity - S)
 	 * The obtained M could be used by the caller to "boost" S.
 	 */
 	if (boost >= 0) {
-		margin  = SCHED_CAPACITY_SCALE - signal;
-		margin *= boost;
+		if (capacity > (long)signal) {
+			margin  = capacity - (long)signal;
+			margin *= boost;
+		}
 	} else
-		margin = -signal * boost;
+		margin = -(long)signal * boost;
 
 	margin  = reciprocal_divide(margin, schedtune_spc_rdiv);
 
@@ -6213,7 +6215,7 @@ schedtune_cpu_margin_with(unsigned long util, int cpu, struct task_struct *p)
 	if (boost == 0)
 		margin = 0;
 	else
-		margin = schedtune_margin(util, boost);
+		margin = schedtune_margin(util, boost, capacity_orig_of(cpu));
 
 	trace_sched_boost_cpu(cpu, util, margin);
 
@@ -6230,7 +6232,7 @@ long schedtune_task_margin(struct task_struct *task)
 		return 0;
 
 	util = task_util_est(task);
-	margin = schedtune_margin(util, boost);
+	margin = schedtune_margin(util, boost, SCHED_CAPACITY_SCALE);
 
 	return margin;
 }
