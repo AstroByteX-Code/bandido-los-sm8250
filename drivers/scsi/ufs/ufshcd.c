c178071c970f853df2b4467234a78d554e7f8613
me <heiler.bemerguy@gmail.com>
2026-07-15 19:12:56 -0300

    scsi: ufs: optimize devfreq polling interval and simple_ondemand threshold

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -1061,7 +1061,7 @@ void SEC_ufs_hpb_rb_count(struct ufs_hba *hba, struct ufshpb_region *rgn)
 #if IS_ENABLED(CONFIG_DEVFREQ_GOV_SIMPLE_ONDEMAND)
 static struct devfreq_simple_ondemand_data ufshcd_ondemand_data = {
 	.upthreshold = 70,
-	.downdifferential = 65,
+	.downdifferential = 15,
 	.simple_scaling = 1,
 };
 
@@ -2545,7 +2545,7 @@ static int ufshcd_devfreq_init(struct ufs_hba *hba)
 	dev_pm_opp_add(hba->dev, clki->min_freq, 0);
 	dev_pm_opp_add(hba->dev, clki->max_freq, 0);
 
-	scaling->profile.polling_ms = 60;
+	scaling->profile.polling_ms = 10;
 	scaling->profile.target = ufshcd_devfreq_target;
 	scaling->profile.get_dev_status = ufshcd_devfreq_get_dev_status;
 
