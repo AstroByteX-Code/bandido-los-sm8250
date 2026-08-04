c26df4bf3b9711e211c1c4468edd6fff360459c3
me <heiler.bemerguy@gmail.com>
2026-08-03 22:59:47 -0300

    lib: lz4: port hybrid ARMv8 NEON assembly acceleration with EROFS margin fallback

--- lib/lz4/lz4.h
+++ lib/lz4/lz4.h
@@ -73,6 +73,8 @@ extern "C" {
 #include <linux/export.h>
 #include <linux/string.h>
 
+#include "lz4armv8/lz4accel.h"
+
 #define LZ4_FORCE_INLINE static inline __attribute__((always_inline))
 
 /*^***************************************************************
