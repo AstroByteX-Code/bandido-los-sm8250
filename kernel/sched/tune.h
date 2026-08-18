68840d39b3bf5c98d509527d76c40e5c3643118d
AstroByteX-Code <codebymianhamza@gmail.com>
2026-08-18 15:26:16 +0500

    sched/debug: stub is_ui_thread and fix Samsung debug/bpf/pinctrl mismatches

--- kernel/sched/tune.h
+++ kernel/sched/tune.h
@@ -17,6 +17,22 @@ int schedtune_task_boost(struct task_struct *tsk);
 
 int schedtune_prefer_idle(struct task_struct *tsk);
 
+void schedtune_enqueue_task(struct task_struct *p, int cpu);
+void schedtune_dequeue_task(struct task_struct *p, int cpu);
+
+#else /* CONFIG_SCHED_TUNE */
+
+#define schedtune_cpu_boost_with(cpu, p)  0
+#define schedtune_task_boost(tsk) 0
+
+#define schedtune_prefer_idle(tsk) 0
+
+#define schedtune_enqueue_task(task, cpu) do { } while (0)
+#define schedtune_dequeue_task(task, cpu) do { } while (0)
+
+#define stune_util(cpu, other_util, walt_load) cpu_util_cfs(cpu_rq(cpu))
+#endif /* CONFIG_SCHED_TUNE */
+
 /*
  * Identifies threads that directly participate in UI frame delivery.
  * Used to apply extra scheduling priority independent of cgroup membership,
@@ -61,22 +77,10 @@ static inline bool is_ui_thread_name(struct task_struct *p)
 
 static inline bool is_ui_thread(struct task_struct *p)
 {
+#ifdef CONFIG_SCHED_WALT
 	return p->is_ui;
+#else
+	return false;
+#endif
 }
 
-
-void schedtune_enqueue_task(struct task_struct *p, int cpu);
-void schedtune_dequeue_task(struct task_struct *p, int cpu);
-
-#else /* CONFIG_SCHED_TUNE */
-
-#define schedtune_cpu_boost_with(cpu, p)  0
-#define schedtune_task_boost(tsk) 0
-
-#define schedtune_prefer_idle(tsk) 0
-
-#define schedtune_enqueue_task(task, cpu) do { } while (0)
-#define schedtune_dequeue_task(task, cpu) do { } while (0)
-
-#define stune_util(cpu, other_util, walt_load) cpu_util_cfs(cpu_rq(cpu))
-#endif /* CONFIG_SCHED_TUNE */
